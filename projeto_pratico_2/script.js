21897da50a5e2df4690bb4059a8a8f96722c368f
devpolatto <polatto.xcode.job@gmail.com>
2023-02-20 23:18:09 -0300

    fix modal error

--- projeto_pratico_2/script.js
+++ projeto_pratico_2/script.js
@@ -5,49 +5,58 @@ const passwordInput = document.querySelector("#password")
 const jobSeletec = document.querySelector("#job")
 const modalAlert = document.querySelector("#modalAlert")
 const errorTextAlert = document.querySelector(".erroText")
-const btnAlertClose = document.querySelector(".btn-alert-close")
+const btnAlertClose = document.querySelector(".btn-close")
 const modalContainer = document.querySelector(".modalContainer")
 
-// btnAlertClose.addEventListener('click', () => {
-//     modalAlert.style.display = "none"
-// })
-
-let erros = []
+let errors = []
 
 function handleModal(errorArray) {
-    modalAlert.style.display = "block"
 
-    errorArray.forEach(erro => {
-        modalContainer.innerHTML += `<p class="erroText">${erro}</p>`
+    modalAlert.style.display = "flex"
+
+    errorArray.map(erro => {
+        modalAlert.innerHTML += `
+        <div class="modalContainer">
+            <p class="erroText">${erro}</p>
+        </div>
+        `
     })
 
-    erros = []
-
-    setTimeout(() => {
-        modalAlert.style.display = "none"
-    }, 3000)
+    if(document.querySelectorAll(".modalContainer").length > 0) {
+        setTimeout(() => {
+            modalAlert.style.display = "none"
+        }, 5000)
+    }
 }
 
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault(); 
+
+    const modalContainerArray = document.querySelectorAll(".modalContainer")
+
+    if(errors.length > 0 && modalContainerArray.length > 0) {
+        errors = []
+        modalContainerArray.forEach.call( modalContainerArray, function(node) {
+            node.parentNode.removeChild(node);
+        });
+    }
 
     // checa se os inputs estao vazios
 
     if(nameInput.value === '') {
-        erros.push('O campo nome não pode ser vazio')
+        errors.push('O campo nome não pode ser vazio')
     }
     if(emailInput.value === '') {
-        erros.push('O campo email não pode ser vazio')
+        errors.push('O campo email não pode ser vazio')
     }
     if(passwordInput.value === '') {
-        erros.push('O campo senha não pode ser vazio')
+        errors.push('O campo senha não pode ser vazio')
     }
 
-    if(erros.length != 0) {
-        handleModal(erros)
-        erros = ['']
-        return console.log(erros)
+    if(errors.length != 0) {
+        handleModal(errors)
+        return console.log(errors)
     }
 
     form.submit()
